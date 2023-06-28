6332f7c7ee7fe589d99cab46e940c5592842eb6b
MehmethP <Mehmetpetek28@gmail.com>
2023-06-28 21:58:50 +0300

    - initial commit, project structure created

---
+++ app/src/main/java/com/example/composeimgflip/app/ComposeImgFlip.kt
@@ -0,0 +1,9 @@
+package com.example.composeimgflip.app
+
+import android.app.Application
+import androidx.compose.runtime.Stable
+import dagger.hilt.android.HiltAndroidApp
+
+@Stable
+@HiltAndroidApp
+class ComposeImgFlip : Application()
